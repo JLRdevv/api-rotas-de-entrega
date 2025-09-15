68df286f12e1c6fe25a1413d210823fc90151368
danichagas <chagasdaniel788@gmail.com>
2025-09-15 18:07:34 -0300

    feat: create point module

---
+++ src/points/points.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { PointsController } from './points.controller'
+import { PointService } from './point.service'
+import { Point } from './entities/point.entity'
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Point])],
+  controllers: [PointsController],
+  providers: [PointService],
+  exports: [PointService]
+})
+export class PointsModule {}
