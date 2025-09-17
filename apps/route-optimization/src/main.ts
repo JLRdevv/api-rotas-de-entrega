78c138034081dc2fa515062d4f7c884aa17e8276
luis <fellipeluis1@gmail.com>
2025-09-17 13:28:04 -0300

    feat: inicia a logica do rabbitMq na Main

--- apps/route-optimization/src/main.ts
+++ apps/route-optimization/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { RouteOptimizationModule } from './route-optimization.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(RouteOptimizationModule);
-    await app.listen(process.env.port ?? 3000);
+    const app = await NestFactory.createMicroservice(RouteOptimizationModule, {
+        transport: Transport.RMQ,
+        options: {
+            urls: ['amqp://rabbitmq:5672'],
+            queue: 'rotas_queue',
+            queueOptions: {},
+        },
+    });
+    await app.listen();
 }
 bootstrap();
