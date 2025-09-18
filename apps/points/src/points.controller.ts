f13889ddf8a734fc963a4fdfb5b0cabfe647c672
Gabriel Troni <troni.gabriel.10@gmail.com>
2025-09-18 16:25:26 -0300

    feat: add health endpoint

--- apps/points/src/points.controller.ts
+++ apps/points/src/points.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { PointService } from './point.service';
 import type {
@@ -56,7 +56,12 @@ export class PointsController {
     }
 
     @MessagePattern({ cmd: 'health' })
-    health(): boolean {
+    healthMessage(): boolean {
+        return true;
+    }
+
+    @Get('health')
+    healthHTTP(): boolean {
         return true;
     }
 }
