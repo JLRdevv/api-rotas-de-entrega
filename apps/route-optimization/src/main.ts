cb4893a8abcc903759d77435bc13b9bfb0bc4f89
lucas-pacheco-01 <lucas.pacheco.nsc@gmail.com>
2025-09-20 22:32:12 -0300

    refactor(): use ConfigService and pino logger in bootstrap

--- apps/route-optimization/src/main.ts
+++ apps/route-optimization/src/main.ts
@@ -1,16 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { RouteOptimizationModule } from './route-optimization.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
+    const appContext = await NestFactory.createApplicationContext(
+        RouteOptimizationModule,
+    );
+
+    const configService = appContext.get(ConfigService);
+    const rmqUrl = configService.get<string>('RMQ_URL');
+    const queueName = 'routes-queue';
+
     const app = await NestFactory.createMicroservice(RouteOptimizationModule, {
         transport: Transport.RMQ,
         options: {
-            urls: ['amqp://rabbitmq:5672'],
-            queue: 'rotas_queue',
-            queueOptions: {},
+            urls: [rmqUrl],
+            queue: queueName,
+            queueOptions: {
+                durable: true,
+            },
         },
     });
+
+    app.useLogger(app.get(Logger));
+
     await app.listen();
+
+    console.log(
+        `Route-optimization microservice is listening on queue: ${queueName}`,
+    );
 }
-bootstrap();
+void bootstrap();
