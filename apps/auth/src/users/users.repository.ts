53ed1292562b20c458f1a249d28cd86a2a9d45a5
unknown <joaolucas3406@gmail.com>
2025-09-22 18:27:05 -0300

    refactor: small refactor on repository

--- apps/auth/src/users/users.repository.ts
+++ apps/auth/src/users/users.repository.ts
@@ -1,17 +1,18 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MongoRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AbstractRepository } from '@app/common';
+import { Logger } from '@nestjs/common';
 
 @Injectable()
 export class UsersRepository extends AbstractRepository<User> {
-  protected readonly logger = new Logger(UsersRepository.name);
+    protected readonly logger = new Logger(UsersRepository.name);
 
-  constructor(
-    @InjectRepository(User)
-    userRepository: MongoRepository<User>,
-  ) {
-    super(userRepository);
-  }
+    constructor(
+        @InjectRepository(User)
+        private readonly repo: MongoRepository<User>,
+    ) {
+        super(repo);
+    }
 }
--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private usersRepository: UsersRepository) {}
+
+    async signup(email: string, password: string) {}
+}
